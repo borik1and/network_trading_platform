4029d3e536a2cf8d6f07552d56a0dff1cce7b72c
boris <borik1and@gmail.com>
2024-04-24 18:25:56 +0300

    delete class role

--- users/models.py
+++ users/models.py
@@ -1,14 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 
 NULLABLE = {'blank': True, 'null': True}
-
-
-class UserRoles(models.TextChoices):
-    MEMBER = 'member', _('member')
-    MODERATOR = 'moderator', _('moderators')
 
 
 class User(AbstractUser):
