1cda2d2e1bb4f554f8d5e028921a1edfdf5429f9
boris <borik1and@gmail.com>
2024-04-26 14:54:54 +0300

    add model retail_network

---
+++ retail_network/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+from factory.models import Factory_contacts
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Retail_network_contacts(models.Model):
+    retail_network_name = models.CharField(max_length=100, default='', verbose_name='Название завода')
+    email = models.EmailField(**NULLABLE)
+    country = models.CharField(max_length=100, default='', verbose_name='страна')
+    city = models.CharField(max_length=100, default='', verbose_name='город')
+    street = models.CharField(max_length=100, default='', verbose_name='улица')
+    house_number = models.CharField(max_length=100, default=0, verbose_name='номер дома')
+
+    def __str__(self):
+        return self.retail_network_name
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
+
+
+class Retail_network_product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя продукта')
+    model = models.CharField(max_length=100, verbose_name='Модель')
+    date_launch = models.DateField(verbose_name='дата выхода продукта на рынок')
+    provider = models.ForeignKey(Factory_contacts, verbose_name='Поставщик', on_delete=models.CASCADE)
+    debt_to_supplier = models.DecimalField(max_digits=20, decimal_places=2, default=0,
+                                           verbose_name='Задолженность перед поставщиком')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
