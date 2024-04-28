5b24a62a92f435a360ef5228612f265622ad38d1
boris <borik1and@gmail.com>
2024-04-28 13:22:35 +0300

    changing code

--- users/management/commands/csuperuser.py
+++ users/management/commands/csuperuser.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
             username="admin",
-            email='admin@edu.com',
+            email='admin@admin.com',
             first_name='admin',
             last_name='root',
             is_staff=True,
