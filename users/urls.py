3d2c0f140039744b70580a90bfff573d099bf07e
boris <borik1and@gmail.com>
2024-04-29 13:56:25 +0300

    update controller to viewsets

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
-from users.views import UserViewSet, UserRegistrationView, UserLoginView
+from users.views import UserViewSet
 
 app_name = 'user'
 
@@ -12,7 +12,5 @@ router.register(r'user', UserViewSet, basename='user')
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('users/register/', UserRegistrationView.as_view(), name='user-registration'),
-    path('users/login/', UserLoginView.as_view(), name='user-login'),
 
 ] + router.urls
--- users/views.py
+++ users/views.py
@@ -1,16 +1,15 @@
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from .serializers import UserSerializer
 from users.models import User
-from users.serializers import UserSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
@@ -22,18 +21,18 @@ class UserViewSet(viewsets.ModelViewSet):
         user.set_password(user.password)
         user.save()
 
+    def get_permissions(self):
+        if self.action in ['create', 'login']:  # Разрешаем только для create и login
+            self.permission_classes = [AllowAny]
+        elif self.action in ['update', 'partial_update', 'destroy']:  # Запрещаем для update, partial_update и destroy
+            self.permission_classes = [IsAuthenticated]
+        return super(UserViewSet, self).get_permissions()
 
-class UserRegistrationView(APIView):
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        # код для входа пользователя
+        return Response("Login Successful")
 
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class UserLoginView(APIView):
-    def post(self, request):
-        return Response("User authenticated successfully", status=status.HTTP_200_OK)
-
+    def create(self, request, *args, **kwargs):
+        # код для создания нового пользователя
+        return super(UserViewSet, self).create(request, *args, **kwargs)
