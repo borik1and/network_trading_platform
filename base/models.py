88e2237106f9b2fec5f6dedc8663a3da60e29d71
boris <borik1and@gmail.com>
2024-05-01 16:33:45 +0300

    add app base

---
+++ base/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+
+
+class BaseContacts(models.Model):
+    name = models.CharField(max_length=100, default='', verbose_name='Имя')
+    email = models.EmailField(**NULLABLE)
+    country = models.CharField(max_length=100, default='', verbose_name='Страна')
+    city = models.CharField(max_length=100, default='', verbose_name='Город')
+    street = models.CharField(max_length=100, default='', verbose_name='Улица')
+    house_number = models.CharField(max_length=100, default=0, verbose_name='Номер дома')
+
+    class Meta:
+        abstract = True
+
+
+class BaseProduct(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя продукта')
+    model = models.CharField(max_length=100, verbose_name='Модель')
+    date_launch = models.DateField(verbose_name='Дата выхода продукта на рынок')
+    debt_to_supplier = models.DecimalField(max_digits=20, decimal_places=2, default=0,
+                                           verbose_name='Задолженность перед поставщиком')
+    creation_date = models.DateTimeField(default=timezone.now, verbose_name='Время создания')
+
+    class Meta:
+        abstract = True
